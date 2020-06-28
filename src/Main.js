9283a7e6993f8cce812a8b7232b014279cada59a
Ike Njoku <ikeenjoku@gmail.com>
2020-06-28 18:12:51 +0100

    setup apollo client

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import Screens from './screens'
+import getEnvVars from '../config'
+
+const { API_URI } = getEnvVars()
+
+const uri = API_URI
+const cache = new InMemoryCache()
+const client = new ApolloClient({
+  uri,
+  cache
+})
 
 export default function Main() {
-  return <Screens />
+  return (
+    <ApolloProvider  client={client}>
+      <Screens />
+    </ApolloProvider>
+  )
 }
