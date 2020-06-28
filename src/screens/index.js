06824631ab19ed34a47810f3a1899a8bb0dd3532
Ike Njoku <ikeenjoku@gmail.com>
2020-06-28 18:19:30 +0100

    upgrade app navigation

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createAppContainer } from 'react-navigation'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
 import { createStackNavigator } from 'react-navigation-stack'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
@@ -8,6 +8,9 @@ import Favorites from './favorites'
 import MyNotes from './mynotes'
 import Feeds from './feeds'
 import Note from './note'
+import AuthLoading from './authloading'
+import Settings from './settings'
+import SignIn from './signin'
 
 const FeedStack = createStackNavigator({
   Feed: Feeds,
@@ -22,6 +25,14 @@ const FavStack = createStackNavigator({
 const MyNoteStack = createStackNavigator({
   MyNotes: MyNotes,
   Note: Note
+})
+
+const AuthStack = createStackNavigator({
+  SignIn: SignIn
+});
+
+const SettingsStack = createStackNavigator({
+  Settings: Settings
 })
 
 const TabNavigator = createBottomTabNavigator({
@@ -45,7 +56,25 @@ const TabNavigator = createBottomTabNavigator({
       tabBarLabel: 'Favorites',
       tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='star' size={24} color={tintColor} />
     }
+  },
+  SettingScreen: {
+    screen: SettingsStack,
+    navigationOptions: {
+      tabBarLabel: 'Settings',
+      tabBarIcon: ({ tintColor }) => (
+        <MaterialCommunityIcons name="settings" size={24} color={tintColor} />
+      )
+    }
   }
+ })
+
+const SwitchNavigator = createSwitchNavigator({
+  AuthLoading: AuthLoading,
+  Auth: AuthStack,
+  App: TabNavigator,
+}, {
+  initialRouteName: 'AuthLoading'
 })
 
-export default createAppContainer(TabNavigator)
+
+export default createAppContainer(SwitchNavigator)
