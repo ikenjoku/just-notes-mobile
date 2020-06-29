2501d1da280c08ec43e0f04aaa01fce86f9d4489
Ike Njoku <ikeenjoku@gmail.com>
2020-06-29 22:12:16 +0100

    fetch favorite notes

--- src/screens/favorites.js
+++ src/screens/favorites.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { useQuery } from '@apollo/client'
+import { GET_MY_FAVORITES } from '../gql/query'
+import { NoteFeed, Loading } from '../components'
 
-function Favorites() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems:'center' }}>
-      <Text>Favorites</Text>
-    </View>
-  )
+function Favorites(props) {
+  const { loading, error, data } = useQuery(GET_MY_FAVORITES)
+
+  if (loading) return <Loading />
+  if (error) return <Text>Error loading your favorites</Text>
+
+  if (data.me.favorites.length !== 0) {
+    return <NoteFeed notes={data.me.favorites} navigation={props.navigation} />
+  } else {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems:'center' }}>
+        <Text>You do not have any favorites yet</Text>
+      </View>
+    )
+  }
 }
 
 Favorites.navigationOptions = {
