985ef30d2b009f070fe93c06e7a2a34ca01ccdce
Ike Njoku <ikeenjoku@gmail.com>
2020-06-29 22:14:26 +0100

    set auth token to context

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client'
 import Screens from './screens'
 import getEnvVars from '../config'
+import * as SecureStore from 'expo-secure-store'
+import { setContext } from 'apollo-link-context'
 
 const { API_URI } = getEnvVars()
 
 const uri = API_URI
+const httpLink = createHttpLink({ uri })
 const cache = new InMemoryCache()
+
+const authLink = setContext(async (_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: (await SecureStore.getItemAsync('token')) || ''
+    }
+  }
+})
+
 const client = new ApolloClient({
   uri,
-  cache
+  cache,
+  link: authLink.concat(httpLink)
 })
 
 export default function Main() {
